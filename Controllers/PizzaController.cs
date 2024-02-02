4274c99163c9a6a4f1a1cf1189257c2534d3f7a5
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-02-02 10:36:25 -0500

    Add delete

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -50,5 +50,16 @@ public class PizzaController : ControllerBase
     return NoContent();
   }
 
-  // DELETE action
+  [HttpDelete("{id}")]
+  public IActionResult Delete(int id)
+  {
+    var pizza = PizzaService.Get(id);
+
+    if (pizza is null)
+      return NotFound();
+
+    PizzaService.Delete(id);
+
+    return NoContent();
+  }
 }
